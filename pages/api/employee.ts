9e164ea3b3488836fd33458d5ef966ce06c3f425
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-17 16:44:23 -0400

    update path on employees endpoint

--- pages/api/employee.ts
+++ pages/api/employee.ts
@@ -29,7 +29,7 @@ export async function getEmployee(eventID?: string, employeeID?: string) {
 }
 
 export async function postEmployee(text: string, eventID?: string) {
-  fs.writeFile("/temp/empleados.csv", text, (err) => {
+  fs.writeFile(path.join("/temp", "empleados.csv"), text, (err) => {
     if (err) throw err;
     console.log("file saved");
   });
@@ -45,7 +45,7 @@ export async function postEmployee(text: string, eventID?: string) {
       "COPY tmp_table(name, email, company, permission, cedula) FROM STDIN DELIMITER ',' CSV HEADER;"
     )
   );
-  const sourceStream = fs.createReadStream("/temp/empleados.csv");
+  const sourceStream = fs.createReadStream(path.join("/temp", "empleados.csv"));
   await pipeline(sourceStream, ingestStream);
   const idsResults: any = await client.query(
     "INSERT INTO employees SELECT * FROM tmp_table ON CONFLICT (cedula) DO UPDATE SET cedula = excluded.cedula RETURNING id;"
