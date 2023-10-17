bcd336fec8e2489c656096da1b1e1d3b9bf87d55
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-17 18:12:16 -0400

    update bseUrl

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ import "dotenv/config";
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://qr-manager-two.vercel.app`
     : "http://localhost:3000";
