0376224b8e3ee094fc8fc5bc8ec7ea0c4d567d09
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-17 10:41:08 -0400

    disabled employee index page

--- pages/employees/index.tsx
+++ pages/employees/index.tsx
@@ -1,23 +1,23 @@
 import Table from "../../components/Table";
 import Layout from "../../components/Layout";
-import getEmployees from "../api/getEmployees";
+
 import ViewData from "../../components/ViewData";
 
 export default function Employees({ employessList }) {
   return (
     <Layout>
-      <section>
+      {/* <section>
         <Table
           data={employessList}
           viewEndpoint="/employees/"
           title="employees"
         />
-      </section>
+      </section> */}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
-  const employessList = await getEmployees();
-  return { props: { employessList } };
-}
+// export async function getServerSideProps() {
+//   // const employessList = await getEmployees();
+//   // return { props: { employessList } };
+// }
