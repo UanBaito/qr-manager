4ca9645ed05c788e539b666fc0bc1d38a4f0d845
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-17 18:05:24 -0400

    update constants to use NEXT_PUBLIC

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ import "dotenv/config";
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000";
