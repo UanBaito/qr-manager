a5a600d0b07a81f85c89daa609c49b69d560d47b
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-22 08:38:25 -0400

    add style to qrcode export button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/home.module.scss";
 import Layout from "../components/Layout";
 import Image from "next/image";
 import qrcode from "../public/qrcode.png";
@@ -7,17 +7,18 @@ import qrcode from "../public/qrcode.png";
 export default function Home() {
   return (
     <Layout>
-      <div className={styles.container}></div>
-      <ul>
-        <li>
-          <button>
-            <figure>
-              <Image src={qrcode} alt="" placeholder="blur" />
-            </figure>
-            <h2>Export QR codes</h2>
-          </button>
-        </li>
-      </ul>
+      <section className={styles.container}>
+        <ul>
+          <li>
+            <button>
+              <figure>
+                <Image src={qrcode} alt="" placeholder="blur" />
+              </figure>
+              <h2>Export QR codes</h2>
+            </button>
+          </li>
+        </ul>
+      </section>
     </Layout>
   );
 }
