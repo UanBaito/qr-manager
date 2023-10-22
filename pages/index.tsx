3e727704e89054076daf0cf6d683f11fcc796a56
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-22 19:53:29 -0400

    update some css and add prefetch on events

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,33 @@ import Head from "next/head";
 import styles from "../styles/home.module.scss";
 import Layout from "../components/Layout";
 import Image from "next/image";
-import qrcode from "../public/qrcode.png";
+import musicLogo from "../public/music_logo.png";
+import { useQuery } from "@tanstack/react-query";
+import { baseUrl } from "../lib/constants";
 
 export default function Home() {
+  useQuery({
+    queryKey: ["events"],
+    queryFn: async () => {
+      const res = await fetch(`${baseUrl}/api/event`);
+      return await res.json();
+    },
+    staleTime: 1000 * 60 * 2,
+  });
+
   return (
     <Layout>
       <section className={styles.container}>
-        <ul>
+        {/* <ul>
           <li>
             <button>
               <figure>
-                <Image src={qrcode} alt="" placeholder="blur" />
+                <Image src={musicLogo} alt="" placeholder="blur" />
               </figure>
-              <h2>Export QR codes</h2>
+              <h2>Create New Event</h2>
             </button>
           </li>
-        </ul>
+        </ul> */}
       </section>
     </Layout>
   );
