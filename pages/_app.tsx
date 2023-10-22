3ad1f9f1f5c501efe080287222077ef4c4e70d72
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-22 08:17:42 -0400

    add spinner component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/global.scss";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
